c4986a63c706f1a5dd8fc89587d52282cdd4b486
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2023-12-27 16:01:21 +0200

    Cleaned up code for search bar

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import styles from './SearchBar.module.css';
+
+function FilterInputs() {
+  function filterFunction(event) {
+    sessionStorage.setItem('FilterChoice', event.target.id);
+  }
+  return (
+    <section className={styles.FilterSection}>
+      <div className={styles.Filter}>
+        <p
+          className={styles.FilterOptions}
+          id="bestMatch"
+          onClick={filterFunction}
+        >
+          Best Match
+        </p>
+        <p
+          className={styles.FilterOptions}
+          id="highestRated"
+          onClick={filterFunction}
+        >
+          Highest Rated
+        </p>
+        <p
+          className={styles.FilterOptions}
+          id="mostReviewed"
+          onClick={filterFunction}
+        >
+          Most Reviewed
+        </p>
+      </div>
+      <hr />
+    </section>
+  );
+}
+
+function SearchInputs() {
+  return (
+    <div className={styles.Inputs}>
+      <input id="searchBusinessTerm" placeholder="Search Businesses" />
+      <input id="searchBusinessArea" placeholder="Where?" />
+    </div>
+  );
+}
+
+function SearchButton() {
+  function searchInfo() {
+    const searchBusinessTerm = document.getElementById('searchBusinessTerm');
+    const searchBusinessArea = document.getElementById('searchBusinessArea');
+
+    const term = searchBusinessTerm.value;
+    const location = searchBusinessArea.value;
+    const radius = 1000;
+    const sortBy = sessionStorage.getItem('filterChoice');
+
+    console.log(`location = ${location}`);
+    console.log(`term = ${term}`);
+    console.log(`radius = ${radius}`);
+    console.log(`sortBy = ${sortBy}`);
+  }
+
+  return <button onClick={searchInfo}>Let's Go</button>;
+}
+
+function SearchBar() {
+  return (
+    <section className={styles.SearchBar}>
+      <FilterInputs />
+      <SearchInputs />
+      <SearchButton />
+    </section>
+  );
+}
+
+export default SearchBar;
